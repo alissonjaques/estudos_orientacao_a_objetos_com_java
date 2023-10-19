b3d6c564500e2d3891c5c0a796f1de46daa83d6b
Alisson Jaques - Computer Engineer <alissonjaquesrmq@gmail.com>
2023-10-19 20:18:13 -0300

    feat: solução desafio condicionais DIO

---
+++ Solucao Exercicios/11 - Exercícios diversos/Solução Desafio/DesafioControleFluxo/src/desafiocontrolefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package desafiocontrolefluxo;
+
+/**
+ *
+ * @author Alisson Jaques
+ */
+public class ParametrosInvalidosException extends IllegalArgumentException {
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
+}
